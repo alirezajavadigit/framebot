b9d5182843050fe1f1e57fb2128b9514cddd373d
alirezajavadi <alirezajavadigit@gmail.com>
2023-12-27 23:00:37 +0330

    index.php is the project starter

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+use activities\Main;
+
+
+//config
+define('TOKEN', '6983736910:AAG6SGL9bjsZgnpyfj9W5Aghv1mauyW6jhA'); // Robot Token
+define('API_URL', 'https://api.telegram.org/bot' . TOKEN . '/'); // the trl this script will request to
+// database config
+define('DB_HOST', 'localhost');
+define('DB_NAME', 'telegram');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '');
+
+
+
+// loading files
+require_once 'activities/Helpers/Helper.php';
+require_once 'vendor/autoload.php';
+
+// call main class to start bot
+$content = file_get_contents("php://input");
+$Main = new Main($content);
