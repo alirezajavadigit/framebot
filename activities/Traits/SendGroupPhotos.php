b97638581278104353cb016e46c5b98a1371421d
alirezajavadi <alirezajavadigit@gmail.com>
2023-12-27 23:04:55 +0330

    send group photos method

---
+++ activities/Traits/SendGroupPhotos.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace activities\Traits;
+
+trait SendGroupPhotos
+{
+    protected function sendGroupPhotos($value)
+    {
+        /*
+        Use this method to send a group of photos, videos, documents or audios as an album. Documents and audio files can be only grouped in an album with messages of the same type. On success, an array of Messages that were sent is returned.
+        */
+        $result = messageRequestJson("sendMediaGroup", $value);
+        return $result;
+    }
+}
