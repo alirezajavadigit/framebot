8c0b3fc520cd715f68661232e3beecfafe71839d
alirezajavadi <alirezajavadigit@gmail.com>
2023-12-27 23:03:31 +0330

    create chat invite link method

---
+++ activities/Traits/CreateChatInviteLink.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace activities\Traits;
+
+trait CreateChaInviteLink
+{
+    /*
+    Use this method to create an additional invite link for a chat. The bot must be an administrator in the chat for this to work and must have the appropriate administrator rights. The link can be revoked using the method revokeChatInviteLink. Returns the new invite link as ChatInviteLink object.
+    */
+    protected function createChatInviteLink($value)
+    {
+        $result = messageRequestJson("createChatInviteLink", $value);
+        return $result;
+    }
+}
