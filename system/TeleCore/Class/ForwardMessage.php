796b8407b459ea257fc732b7bf38395c20a33094
alirezajawadi <alirezajavadigit@gmail.com>
2024-05-13 23:25:13 +0330

    Implement ForwardMessage class
    Created ForwardMessage, a class representing Telegram bot messages with parameter-setting methods. It auto-forwards messages upon object destruction, preventing duplicates.

---
+++ system/TeleCore/Class/ForwardMessage.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Framebot
+|--------------------------------------------------------------------------
+| PHP framework for Telegram bot development. Fast, flexible, feature-rich.
+|--------------------------------------------------------------------------
+| @category  Framework
+| @author    Alireza Javadi
+| @license   MIT License
+| @link      https://github.com/alirezajavadigit/framebot
+|--------------------------------------------------------------------------
+| System\TeleCore\Class\Message
+|--------------------------------------------------------------------------
+| This class represents a message in the Telegram bot system and provides methods
+| for setting various parameters of the message. It automatically sends the
+| message upon destruction of the object.
+|--------------------------------------------------------------------------
+*/
+
+namespace System\TeleCore\Class;
+
+use System\TeleCore\Traits\Caller;
+use System\TeleCore\Traits\Request;
+use System\TeleCore\Traits\Setters;
+
+class ForwardMessage
+{
+    use Request, Caller, Setters;
+
+    /**
+     * @var array $params Array to store message parameters.
+     */
+    private $params; // Array to store message parameters.
+
+    /**
+     * @var bool $ended Flag to indicate if the message has been sent.
+     */
+    private $ended = false; // Flag to indicate if the message has been sent.
+
+    /**
+     * Forward the message with the set parameters.
+     *
+     * This method forwards the message using the request method from the Request trait
+     * with the parameters set in this class. It is automatically called upon destruction
+     * of the Message object.
+     *
+     * @return void
+     */
+    public function __destruct()
+    {
+        // Check if the message hasn't already been sent to avoid duplicate sending.
+        if (!$this->ended) {
+            // Send the message using the request method with the set parameters.
+            $this->request("forwardMessage", $this->params);
+        }
+    }
+
+    /**
+     * Set the end flag and send the message.
+     *
+     * This method sets the end flag to true, indicating that the message has been sent,
+     * and then sends the message using the request method with the set parameters.
+     *
+     * @return mixed The result of sending the message.
+     */
+    private function setEnd()
+    {
+        // Set the end flag to true.
+        $this->ended = true;
+        // Send the message using the request method with the set parameters.
+        return $this->request("forwardMessage", $this->params);
+    }
+}
