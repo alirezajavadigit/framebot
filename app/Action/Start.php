098f8c551d9932251a7cde4bde7fd70c9102e8ce
alirezajawadi <alirezajavadigit@gmail.com>
2024-05-13 21:11:17 +0330

    RePath Start.php
    repath the start.php trait for better organization and readability.

---
+++ app/Action/Start.php
@@ -0,0 +1,39 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Framebot
+|--------------------------------------------------------------------------
+| PHP framework for Telegram bot development. Fast, flexible, feature-rich.
+|--------------------------------------------------------------------------
+| @category  Framework
+| @framework Framebot
+| @version   2.3.0
+| @author    Alireza Javadi
+| @license   MIT License
+| @link      https://github.com/alirezajavadigit/framebot
+|--------------------------------------------------------------------------
+| trait Start
+|--------------------------------------------------------------------------
+| This trait contains methods related to handling the "/start" command.
+|--------------------------------------------------------------------------
+*/
+
+namespace App\Action;
+
+use System\TeleCore\Class\Message;
+
+trait Start
+{
+    /**
+     * Start method to handle the "/start" command.
+     * Sends a welcome message to the user.
+     *
+     * @return void
+     */
+    protected function start()
+    {
+        // Send a message to the user containing a welcome greeting.
+        Message::chatId($this->chatID)->text("hi thank you for using my framework renew All");
+    }
+}
