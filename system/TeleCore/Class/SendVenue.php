bc248f801edb3570b94a4badd2b006d20c7fe014
alirezajavadigit <alirezajavadigit@gmail.com>
2025-02-14 00:29:52 +0330

    :white_check_mark: feat(TeleCore): add SendVenue class with auto-send on destruct

---
+++ system/TeleCore/Class/SendVenue.php
@@ -0,0 +1,75 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Framebot
+|--------------------------------------------------------------------------
+| PHP framework for Telegram bot development. Fast, flexible, feature-rich.
+|--------------------------------------------------------------------------
+| @category  Framework
+| @author    Alireza Javadi
+| @license   MIT License
+| @link      https://github.com/alirezajavadigit/framebot
+|--------------------------------------------------------------------------
+| System\TeleCore\Class\SendVenue
+|--------------------------------------------------------------------------
+| This class represents a venue message in the Telegram bot system and provides
+| methods for setting various parameters of the venue. It automatically
+| sends the venue upon destruction of the object.
+|--------------------------------------------------------------------------
+*/
+
+namespace System\TeleCore\Class;
+
+use System\TeleCore\Traits\Caller;
+use System\TeleCore\Traits\Request;
+use System\TeleCore\Traits\Setters;
+
+class SendVenue
+{
+    use Request, Caller, Setters;
+
+    /**
+     * @var array $params Array to store message parameters.
+     */
+    private $params; // Array to store message parameters.
+
+    /**
+     * @var bool $ended Flag to indicate if the venue has been sent.
+     */
+    private $ended = false; // Flag to indicate if the venue has been sent.
+
+    /**
+     * Send the venue with the set parameters.
+     *
+     * This method sends the venue using the request method from the Request trait
+     * with the parameters set in this class. It is automatically called upon destruction
+     * of the SendVenue object.
+     *
+     * @return void
+     */
+    public function __destruct()
+    {
+        // Check if the venue hasn't already been sent to avoid duplicate sending.
+        if (!$this->ended) {
+            // Send the venue using the request method with the set parameters.
+            $this->request("sendVenue", $this->params);
+        }
+    }
+
+    /**
+     * Set the end flag and send the venue.
+     *
+     * This method sets the end flag to true, indicating that the venue has been sent,
+     * and then sends the venue using the request method with the set parameters.
+     *
+     * @return mixed The result of sending the venue.
+     */
+    private function setEnd()
+    {
+        // Set the end flag to true.
+        $this->ended = true;
+        // Send the venue using the request method with the set parameters.
+        return $this->request("sendVenue", $this->params);
+    }
+}
